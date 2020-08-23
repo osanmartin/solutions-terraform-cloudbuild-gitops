49021f0aabc762672c81e5770a7639ce50bedf28
Orlando San Martín <orlando.sanmartin@zentagroup.com>
2020-08-23 01:41:41 -0400

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "bucket-osanmartin"
     prefix = "env/dev"
   }
 }
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "terraform-test-osanmartin-tfstate"
     prefix = "env/prod"
   }
 }
